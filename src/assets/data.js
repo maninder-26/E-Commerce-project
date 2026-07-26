bee5f58ba7f21e2479f66246e72cc7ad8c624cfd
maninder-26 <manindersingh1079@gmail.com>
2026-07-26 10:45:15 -0700

    Fix image import paths in data.js

--- src/assets/data.js
+++ src/assets/data.js
@@ -1,7 +1,7 @@
-import p1_img from '../assets/p1.png'
-import p2_img from '../assets/p2.png'
-import p3_img from '../assets/p3.avif'
-import p4_img from '../assets/p4.png'
+import p1_img from "./p1.png";
+import p2_img from './p2.png'
+import p3_img from './p3.avif'
+import p4_img from './p4.png'
 
 let data_product= [
      {
